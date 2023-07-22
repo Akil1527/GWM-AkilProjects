91ba6bd879046d4ab04b2b5890b5966892ce29cc
Akil1527 <138638538+Akil1527@users.noreply.github.com>
2023-07-22 17:19:32 +0530

    Add files via upload

---
+++ TextEditorProject4/src/com/project4/test/Test.java
@@ -0,0 +1,12 @@
+package com.project4.test;
+
+import com.project4.TextEditor;
+
+public class Test {
+
+	public static void main(String[] args) {
+	TextEditor textEditor = new TextEditor();
+
+	}
+
+}
